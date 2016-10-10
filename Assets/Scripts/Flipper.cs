c70e3b5a9aa2d3d1d79522d414e81a54c3cfcd4f
Konstantin <sqb@mail.ru>
2016-10-10 12:13:38 +0300

    Basic flipper logic added

---
+++ Assets/Scripts/Flipper.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace ConstantineSpace.PinBall
+{
+    public class Flipper : MonoBehaviour
+    {
+        private HingeJoint2D _hingeJoint2D;
+        /// <summary>
+        ///     Initialization.
+        /// </summary>
+        public void Start()
+        {
+            _hingeJoint2D = GetComponent<HingeJoint2D>();
+        }
+
+        /// <summary>
+        ///     Rotates up the flipper.
+        /// </summary>
+        private void OnMouseDown()
+        {
+            _hingeJoint2D.useMotor = true;
+        }
+
+        /// <summary>
+        ///     Checks position and rotates the flipper down.
+        /// </summary>
+        public void Update()
+        {
+            if (_hingeJoint2D.limits.max > 0 && _hingeJoint2D.limitState == JointLimitState2D.UpperLimit)
+            {
+                _hingeJoint2D.useMotor = false;
+            }
+            else if (_hingeJoint2D.limits.max < 0 && _hingeJoint2D.limitState == JointLimitState2D.LowerLimit)
+            {
+                _hingeJoint2D.useMotor = false;
+            }
+        }
+    }
+}
