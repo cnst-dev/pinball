41d3aea878cf396c8d1db5c4ed5f10915bfdd522
Konstantin <sqb@mail.ru>
2016-10-12 13:55:07 +0300

    Auto rotation added

---
+++ Assets/Scripts/Tools/AutoRotate.cs
@@ -0,0 +1,22 @@
+﻿using ConstantineSpace.PinBall;
+using UnityEngine;
+
+namespace ConstantineSpace.Tools
+{
+    /// <summary>
+    ///     Use this class to make GameObject rotation.
+    /// </summary>
+    public class AutoRotate : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector3 _rotationSpeed = new Vector3(0.0f, 0.0f, 5.0f);
+
+        private void Update()
+        {
+            if (GameManager.Instance.CurrentState == GameManager.GameState.InGame)
+            {
+                transform.Rotate(_rotationSpeed);
+            }
+        }
+    }
+}
